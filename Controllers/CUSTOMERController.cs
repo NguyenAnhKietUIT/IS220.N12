253d22a57acd82dec820b4e789a33b5a04e03004
Nguyễn Đạt <84862816+DT47UIT@users.noreply.github.com>
2022-12-07 15:25:51 +0700

    Revert "update"
    This reverts commit ea3ae4be3aaf913d73e0dd2918f665880698c12b.

--- Controllers/CUSTOMERController.cs
+++ Controllers/CUSTOMERController.cs
@@ -13,19 +13,6 @@ namespace IS220.N12.Controllers
         {
             return View();
         }
-        public ActionResult ViewAllBooking()
-        {
-            return View();
-        }
-        public ActionResult YourAccount()
-        {
-            return View();
-        }
-
-        public ActionResult Favourites()
-        {
-            return View();
-        }
 
         // GET: CUSTOMER/Details/5
         public ActionResult Details(int id)
