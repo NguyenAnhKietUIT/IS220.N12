4f87aaf842131db7e68bb6e4bf6d4bdbd18a65f0
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2022-12-10 16:28:47 +0700

    update home page

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace IS220.N12
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Homepage", action = "Index", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
